ef6c06ba1ab62fc263a656e83054b91fd5a6128f
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-01-24 14:40:16 -0300

    add database.js and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default function (request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function (request, response) {
+    const result = await database.query("SELECT 1 + 1 as sum;");
+
+    const sum = result.rows[0].sum;
+
     response.status(200).json({
-        hello: "world",
+        sum,
     });
 }
--- test/pages/api/v1/status/get.test.js
+++ test/pages/api/v1/status/get.test.js
@@ -1,5 +1,8 @@
 test("should return status 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
 
+    const { sum } = await response.json();
+
     expect(response.status).toBe(200);
+    expect(sum).toBe(2);
 });
