652fa1b47d7e9a31e83f56ad8b2b91c6f4f6354e
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-01-26 13:05:35 -0300

    add /status route

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,30 @@
 import database from "infra/database.js";
 
 export default async function (request, response) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
+    const createdAt = new Date().toISOString();
 
-    const sum = result.rows[0].sum;
+    const { rows } = await database.query({
+        text: `
+       SELECT
+            current_setting('server_version')::float AS version,
+            (SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1) AS active_connections,
+            (SELECT current_setting('max_connections')::int) AS max_connections;
+    `,
+        values: [process.env.POSTGRES_DB],
+    });
+
+    const version = rows[0].version;
+    const max_connections = rows[0].max_connections;
+    const active_connections = rows[0].active_connections;
 
     response.status(200).json({
-        sum,
+        updated_at: createdAt,
+        dependencies: {
+            database: {
+                version,
+                max_connections,
+                active_connections,
+            },
+        },
     });
 }
--- test/pages/api/v1/status/get.test.js
+++ test/pages/api/v1/status/get.test.js
@@ -1,8 +1,26 @@
 test("should return status 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
 
-    const { sum } = await response.json();
+    const body = await response.json();
 
-    expect(response.status).toBe(200);
-    expect(sum).toBe(2);
+    const parsedUpdatedAt = new Date(body.updated_at);
+    const now = new Date();
+
+    expect(body.updated_at).toEqual(parsedUpdatedAt.toISOString());
+    expect(now.getDate()).toEqual(parsedUpdatedAt.getDate());
+    expect(now.getMonth()).toEqual(parsedUpdatedAt.getMonth());
+    expect(now.getYear()).toEqual(parsedUpdatedAt.getYear());
+
+    console.log(body);
+
+    expect(body).toEqual({
+        updated_at: expect.stringContaining(parsedUpdatedAt.toISOString()),
+        dependencies: {
+            database: {
+                version: 16.6,
+                max_connections: 100,
+                active_connections: 1,
+            },
+        },
+    });
 });
