da7b9952a16125a882ff8f72e27868dac441197b
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-02-12 16:41:47 -0300

    refactor: change the delay() from the start of script

--- infra/scripts/wait-for-next.js
+++ infra/scripts/wait-for-next.js
@@ -5,39 +5,41 @@ const red = "\x1b[31m";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-
 const print = (message, color) => {
-    process.stdout.write(
-        `${color} ${message} ${reset}\n\n`,
-    );
-}
+    process.stdout.write(`${color} ${message} ${reset}\n\n`);
+};
 const verifyNextActivity = async ({ tries = 50, timePerTry = 1000 } = {}) => {
-    print('Aguardando servidor next iniciar', yellow)
+    print("Aguardando servidor next iniciar...", yellow);
 
     for (let i = 0; i < tries; i++) {
+        await delay(timePerTry);
         try {
             const response = await fetch("http://localhost:3000/api/v1/status");
 
             if (response.ok) {
-                print('Servidor iniciado', green)
+                print("Servidor iniciado", green);
                 return;
             } else {
-                print(`${response.status}:${response.statusText} - ${response.url}`, yellow)
+                print(
+                    `${response.status}:${response.statusText} - ${response.url}...`,
+                    yellow,
+                );
             }
-
         } catch (error) {
-            print(`Error: ${error.message} ${yellow} cause: ${error.cause}`, yellow)
+            print(
+                `Error: ${error.message} ${yellow} cause: ${error.cause}`,
+                yellow,
+            );
         }
-        await delay(timePerTry);
     }
-    print(`Número máximo de tentativas excedido. Max:${tries}`, red)
+    print(`Número máximo de tentativas excedido. Max:${tries}`, red);
 };
 
 (async () => {
     try {
         await verifyNextActivity();
     } catch (error) {
-        console.error('Erro:', error);
+        console.error("Erro:", error);
         process.exit(1);
     }
 })();
