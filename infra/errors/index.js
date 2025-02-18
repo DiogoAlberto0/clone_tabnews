6f3d08771566a2428f013ef132fbe0aa01c9a044
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-02-18 09:19:27 -0300

    feat: add `InternalServerError` class

---
+++ infra/errors/index.js
@@ -0,0 +1,9 @@
+export class InternalServerError extends Error {
+    constructor({ cause }) {
+        super("Internal Server Error");
+        (this.name = "InternalServerError"),
+            (this.action = "Contact the suport"),
+            (this.statusCode = 500),
+            (this.cause = cause);
+    }
+}
